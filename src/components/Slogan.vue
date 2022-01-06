6b619bc9fa36ea2c5446a9ac9be633338859f065
sacha <sacha.blin-zwertvaegher@epitech.eu>
2022-01-06 10:57:44 +0100

    Added project

---
+++ src/components/Slogan.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="slogan">
+        <h1 class="font-weight-bold">{{ msg }}</h1>
+        <h1 class="font-weight-bold">{{ msg2 }}</h1>
+        <p>{{ auteur }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Slogan',
+    props: {
+        msg: String,
+        msg2: String,
+        auteur: String,
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Rokkitt:wght@400;800&display=swap");
+h1 {
+    font-family: "Rokkitt", serif;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+
+.slogan {
+    background-color: rgba(255, 255, 255, 0.397);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <div class="content">
+      <Slogan
+        class="bold"
+        msg="Mollecular Gastronomy"
+        msg2="Make it SIMPLE"
+        auteur="By Thierry MARX"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Navbar from '@/components/Navbar.vue'
+import Slogan from '@/components/Slogan.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar,
+    Slogan
+  }
+}
+</script>
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Rokkitt&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Rokkitt:wght@400;800&display=swap");
+.content {
+  background-image: url("../assets/fondchimievertrose.jpg");
+  background-size: 100vh;
+  background-repeat: no-repeat;
+  font-family: "Rokkitt", serif;
+}
+.bold {
+  font-family: "Rokkitt", serif;
+  font-style: bold;
+}
+</style>
